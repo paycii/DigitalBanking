78308cb5b619fc47a1d5a02720ad197b0ab19d17
Ilyas <ilyas.sadiqui@gmail.com>
2022-06-27 17:56:25 +0100

    sc

--- digitalbankingui/digital-banking-web/src/app/navbar/navbar.component.ts
+++ digitalbankingui/digital-banking-web/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,8 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
